00eff662ac2b8f6ee8091116edc7a6fce7e3df04
praveenmunukutla <89352907+praveenmunukutla@users.noreply.github.com>
2025-09-07 23:52:31 +0530

    Time: 28 ms (15.39%), Space: 21.8 MB (8.01%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if(head == nullptr)
+            return nullptr;
+        
+        Node* curr = head;
+        Node* temp = nullptr;
+        while(curr){
+            temp = curr->next;
+            curr->next = new Node(curr->val);
+            curr->next->next = temp;
+            curr = temp;
+        }
+        curr = head;
+        while(curr){
+            curr->next->random = curr->random ? curr->random->next : curr->random;
+            curr = curr->next->next;
+        }
+        
+        Node* original = head, *copy = head->next;
+        temp = copy;
+        while(original && copy){
+            original->next = original->next ? original->next->next : original->next;
+            copy->next = copy->next ? copy->next->next : copy->next;
+            original = original->next;
+            copy = copy->next;
+        }
+        return temp;
+    }
+};
